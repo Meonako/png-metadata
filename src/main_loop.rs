67013a8b4d8145cd945e3e616f29e6b73f5a79b2
Meonako <76484203+Meonako@users.noreply.github.com>
2023-03-04 18:03:01 +0700

    Move: match arm handle to seperate function

--- src/main_loop.rs
+++ src/main_loop.rs
@@ -1,4 +1,5 @@
-use std::{io::Write, fs::File};
+use std::io::Write;
+use std::fs::File;
 
 use colored::Colorize;
 
@@ -25,58 +26,18 @@ pub fn start(client: reqwest::blocking::Client, mut args: Vec<String>) {
         let new_file = 
             match user_input.to_lowercase().trim() {
                 str if str.starts_with("http") => {
-                    let mut req = client.get(str);
-
-                    if str.contains("i.pximg.net") {
-                        req = req.header("Referer", "https://www.pixiv.net/")
-                    }
-
-                    let response = match req.send() {
-                        Ok(x) => x,
-                        Err(e) => {
-                            println!(
-                                "{}\n{}",
-                                e.to_string().red(),
-                                "Sometimes, \"reqwest\"/\"hyper\" just randomly spit out error"
-                                    .yellow()
-                            );
-                            continue;
-                        }
-                    };
-
-                    if !response.status().is_success() {
-                        println!(
-                            "Status: {}\nData: {}",
-                            response.status().as_str().red(),
-                            response.text().unwrap()
-                        );
+                    if let Some(file) = download_file(&client, str) {
+                        file
+                    } else {
                         continue;
                     }
-
-                    let image_data = response.bytes().unwrap();
-
-                    File::create(TEMP_FILE_NAME)
-                        .unwrap()
-                        .write_all(&image_data)
-                        .unwrap();
-
-                    File::open(TEMP_FILE_NAME).unwrap()
                 }
                 str if str.starts_with("file:") => {
-                    let path_string = str.replace("file:", "");
-                    let path = std::path::Path::new(&path_string);
-
-                    if !path.exists() {
-                        println!(
-                            "{}{}{}",
-                            "File (".red(),
-                            path_string.cyan(),
-                            ") not found!".red()
-                        );
+                    if let Some(file) = open_file(str) {
+                        file
+                    } else {
                         continue;
                     }
-
-                    File::open(path).unwrap()
                 }
                 "quit" | "stop" => {
                     remove_temp_file();
@@ -115,5 +76,66 @@ pub fn start(client: reqwest::blocking::Client, mut args: Vec<String>) {
         }
 
         remove_temp_file();
+    }
+}
+
+fn download_file(client: &reqwest::blocking::Client, url: &str) -> Option<File> {
+    let mut req = client.get(url);
+
+    if url.contains("i.pximg.net") {
+        req = req.header("Referer", "https://www.pixiv.net/")
+    }
+
+    let response = match req.send() {
+        Ok(x) => x,
+        Err(e) => {
+            println!(
+                "{}\n{}",
+                e.to_string().red(),
+                "Sometimes, \"reqwest\"/\"hyper\" just randomly spit out error".yellow()
+            );
+            return None;
+        }
+    };
+
+    if !response.status().is_success() {
+        println!(
+            "Status: {}\nData: {}",
+            response.status().as_str().red(),
+            response.text().unwrap()
+        );
+        return None;
+    }
+
+    let image_data = response.bytes().unwrap();
+
+    File::create(TEMP_FILE_NAME)
+        .unwrap()
+        .write_all(&image_data)
+        .unwrap();
+
+    Some(File::open(TEMP_FILE_NAME).unwrap())
+}
+
+fn open_file(file_name: &str) -> Option<File> {
+    let mut path_string = file_name.replace("file:", "");
+
+    if path_string.starts_with("\"") && path_string.ends_with("\"") {
+        // Remove first and last double quote
+        path_string = path_string[1..path_string.len() - 1].to_string()
+    }
+
+    let path = std::path::Path::new(&path_string);
+
+    if !path.exists() {
+        println!(
+            "{}{}{}",
+            "File (".red(),
+            path_string.cyan(),
+            ") not found!".red()
+        );
+        None
+    } else {
+        Some(File::open(path).unwrap())
     }
 }
